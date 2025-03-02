8abe6fc7a997067376295ea8ea365bad15d0a552
xMcacutt <matthew.cacutt@gmail.com>
2025-03-02 16:41:13 +0000

    updated publish scrript

--- publish.bat
+++ publish.bat
@@ -5,6 +5,7 @@ setlocal
 set FILE1=mod_info.json
 set FILE2=favico.ico
 set FILE3=Release\Ty1AP-Client.dll
+set FILE4=Patch_PC.rkv
 
 :: Define destination directory
 set DEST=Publish
@@ -19,6 +20,7 @@ echo Copying files to %DEST%...
 copy "%FILE1%" "%DEST%"
 copy "%FILE2%" "%DEST%"
 copy "%FILE3%" "%DEST%"
+copy "%FILE4%" "%DEST%"
 
 echo All files copied successfully!
 endlocal
